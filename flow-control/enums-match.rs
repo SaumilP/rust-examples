b9055241ee47ee84038b50d9157f6b368327b829
Saumil Patel <saumilpatel1@gmail.com>
2017-07-03 19:13:12 +0200

    more flow control structure examples added

---
+++ flow-control/enums-match.rs
@@ -0,0 +1,33 @@
+// `allow` required to silence warnings because only one variant is used.
+#[allow(dead_code)]
+enum Color {
+    Red,
+    Blue,
+    Green,
+
+    RGB(u32, u32, u32),
+    HSV(u32, u32, u32),
+    HSL(u32, u32, u32),
+    CMY(u32, u32, u32),
+}
+
+fn main() {
+    let color = Color::RGB(122, 17, 40);
+    // TODO ^ Try different variants for `color`
+
+    println!("What color is it?");
+    // An `enum` can be destructured using a `match`.
+    match color {
+        Color::Red   => println!("The color is Red!"),
+        Color::Blue  => println!("The color is Blue!"),
+        Color::Green => println!("The color is Green!"),
+        Color::RGB(r, g, b) =>
+            println!("Red: {}, green: {}, and blue: {}!", r, g, b),
+        Color::HSV(h, s, v) =>
+            println!("Hue: {}, saturation: {}, value: {}!", h, s, v),
+        Color::HSL(h, s, l) =>
+            println!("Hue: {}, saturation: {}, lightness: {}!", h, s, l),
+        Color::CMY(c, m, y) =>
+            println!("Cyan: {}, magenta: {}, yellow: {}!", c, m, y),
+    }
+}
